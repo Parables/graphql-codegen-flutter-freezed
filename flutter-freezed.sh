ac106c62279718e34f13ab7718fb00e2cf65e8c5
parables <38154990+Parables@users.noreply.github.com>
2022-08-05 04:58:03 +0000

    fix: :ambulance: fixed bash command location issue

--- flutter-freezed.sh
+++ flutter-freezed.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-npm run gen -- "$@"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+echo "$DIR"
+
+SCRIPT_PATH="${BASH_SOURCE}"
+while [ -L "${SCRIPT_PATH}" ]; do
+    SCRIPT_DIR="$(cd -P "$(dirname "${SCRIPT_PATH}")" >/dev/null 2>&1 && pwd)"
+    SCRIPT_PATH="$(readlink "${SCRIPT_PATH}")"
+    [[ ${SCRIPT_PATH} != /* ]] && SCRIPT_PATH="${SCRIPT_DIR}/${SCRIPT_PATH}"
+done
+SCRIPT_PATH="$(readlink -f "${SCRIPT_PATH}")"
+SCRIPT_DIR="$(cd -P "$(dirname -- "${SCRIPT_PATH}")" >/dev/null 2>&1 && pwd)"
+
+echo "target dir:==> ${DIR}"
+echo "Script dir:==> ${SCRIPT_DIR}"
+cd "$SCRIPT_DIR" || exit
+
+npm run gen -- --config "$DIR/codegen.yml" "$@"
