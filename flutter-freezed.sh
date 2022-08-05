64419e89f8957f68b378ec3181ac2c85e094637a
parables <38154990+Parables@users.noreply.github.com>
2022-08-05 06:29:29 +0000

    fix: :ambulance: reorder scripts to get the location of the process first

--- flutter-freezed.sh
+++ flutter-freezed.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-echo "$DIR"
-
+# get the location of the process
 SCRIPT_PATH="${BASH_SOURCE}"
 while [ -L "${SCRIPT_PATH}" ]; do
     SCRIPT_DIR="$(cd -P "$(dirname "${SCRIPT_PATH}")" >/dev/null 2>&1 && pwd)"
@@ -13,8 +10,15 @@ done
 SCRIPT_PATH="$(readlink -f "${SCRIPT_PATH}")"
 SCRIPT_DIR="$(cd -P "$(dirname -- "${SCRIPT_PATH}")" >/dev/null 2>&1 && pwd)"
 
-echo "target dir:==> ${DIR}"
-echo "Script dir:==> ${SCRIPT_DIR}"
+# get the current working directory
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo "cwd    dir:==> ${DIR}"
+
+# change into the process location so that it can use the `package.json` file there
 cd "$SCRIPT_DIR" || exit
 
+echo "target dir:==> ${DIR}"
+echo "Script dir:==> ${SCRIPT_DIR}"
+
+# fire the generator
 npm run gen -- --config "$DIR/codegen.yml" "$@"
